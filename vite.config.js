2c2384f3939686f4c6f0eb4d68548d1a054bc512
kingdom <kingdomogbeta@gmail.com>
2025-12-06 23:36:48 +0100

    Setup GitHub Actions for proper deployment and fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/broker-project/', // Change 'broker-project' to your repo name
+  base: process.env.VITE_APP_BASE || '/',
 })
