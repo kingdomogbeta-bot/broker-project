603256f14e40aaefadcfeca6ebe0f19d1976a89b
kingdom <kingdomogbeta@gmail.com>
2025-12-06 23:11:35 +0100

    Update for GitHub Pages deployment with base path

--- Brocker/vite.config.js
+++ Brocker/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/broker-project/', // Change 'broker-project' to your repo name
 })
